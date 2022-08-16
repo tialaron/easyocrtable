f1775ba0a3076369842f567fd46cf33cbb198abc
tialaron <30555530+tialaron@users.noreply.github.com>
2022-08-16 13:04:12 +0300

    Update easyocrtable1.py

--- easyocrtable1.py
+++ easyocrtable1.py
@@ -84,8 +84,8 @@ if is_clicked2:
             st.write(text2)
             nlp1 = spacy.load('ru_core_news_sm')
             doc1 = nlp1(text2)
-            st.write(doc1)
+            #st.write(doc1)
             ent_html = displacy.render(doc1, style="ent", jupyter=False)
      
-            st.markdown(ent_html, unsafe_allow_html=True)
-            st.write(type(ent_html))
+            #st.markdown(ent_html, unsafe_allow_html=True)
+            #st.write(type(ent_html))
