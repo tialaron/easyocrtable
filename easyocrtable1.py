7addee426f88082915bfddee3f3d52e7ae3c77be
tialaron <30555530+tialaron@users.noreply.github.com>
2022-08-16 12:05:18 +0300

    Update easyocrtable1.py

--- easyocrtable1.py
+++ easyocrtable1.py
@@ -79,6 +79,8 @@ if is_clicked2:
                                     if  bounds[i][j] :
                                                 text1 = text1 + str(bounds[i][j]) + '\n'
             st.write(text1)
+            st.write(text1[5])
+                        
             text2 = text1.replace('nan', '') 
             
             nlp1 = spacy.load('ru_core_news_sm')
